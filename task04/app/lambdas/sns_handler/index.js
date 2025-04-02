b13459b50830402c1858f30501fca09616dbbfbc
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-02 11:52:55 +0530

    final-commit

---
+++ task04/app/lambdas/sns_handler/index.js
@@ -0,0 +1,23 @@
+const util = require('util');
+
+/**
+ * CloudWatch Log Handler for SNS messages.
+ */
+exports.handler = async (event) => {
+    try {
+        // Log the contents of SNS messages
+        event.Records.forEach(record => {
+            const snsMessage = record.Sns.Message || '';
+            console.log(`SNS Message Content: ${snsMessage}`);
+        });
+
+        return {
+            statusCode: 200,
+            body: 'SNS Message successfully logged.',
+        };
+    } catch (error) {
+        console.error(`An error occurred while processing SNS messages: ${error.message}`);
+        throw error;
+    }
+};
+ 
---
+++ task04/app/lambdas/sqs_handler/index.js
@@ -0,0 +1,23 @@
+const util = require('util');
+
+/**
+ * CloudWatch Log Handler for SQS messages.
+ */
+exports.handler = async (event) => {
+    try {
+        // Log the contents of each message in the SQS batch
+        event.Records.forEach(record => {
+            const messageBody = record.body || '';
+            console.log(`SQS Message Body: ${messageBody}`);
+        });
+
+        return {
+            statusCode: 200,
+            body: 'SQS Message successfully logged.',
+        };
+    } catch (error) {
+        console.error(`An error occurred while processing SQS messages: ${error.message}`);
+        throw error;
+    }
+};
+ 
