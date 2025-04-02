7ce8df4c13f5e886a610097ac96f105b973eb0a4
Swetaparna Mishra <swetaparna_mishra@epam.com>
2025-04-02 12:08:54 +0530

    final-commit

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,23 +1,13 @@
-const util = require('util');
-
-/**
- * CloudWatch Log Handler for SNS messages.
- */
 exports.handler = async (event) => {
-    try {
-        // Log the contents of SNS messages
-        event.Records.forEach(record => {
-            const snsMessage = record.Sns.Message || '';
-            console.log(`SNS Message Content: ${snsMessage}`);
-        });
-
-        return {
-            statusCode: 200,
-            body: 'SNS Message successfully logged.',
-        };
-    } catch (error) {
-        console.error(`An error occurred while processing SNS messages: ${error.message}`);
-        throw error;
-    }
-};
+    console.log("SNS Event Received:", JSON.stringify(event, null, 2));
  
+    event.Records.forEach(record => {
+        const snsMessage = record.Sns.Message;
+        console.log("Message from SNS:", snsMessage);
+    });
+    // return
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "SNS Event Processed" })
+    };
+};
--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,23 +1,13 @@
-const util = require('util');
-
-/**
- * CloudWatch Log Handler for SQS messages.
- */
 exports.handler = async (event) => {
-    try {
-        // Log the contents of each message in the SQS batch
-        event.Records.forEach(record => {
-            const messageBody = record.body || '';
-            console.log(`SQS Message Body: ${messageBody}`);
-        });
-
-        return {
-            statusCode: 200,
-            body: 'SQS Message successfully logged.',
-        };
-    } catch (error) {
-        console.error(`An error occurred while processing SQS messages: ${error.message}`);
-        throw error;
-    }
-};
+    console.log("SQS Event Received:", JSON.stringify(event, null, 2));
  
+    event.Records.forEach(record => {
+        const snsMessage = record.Sns.Message;
+        console.log("Message from SQS:", snsMessage);
+    });
+ 
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "SQS Event Processed" })
+    };
+};
